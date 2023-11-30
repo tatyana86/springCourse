a97c38168ea3ffe06f203efd8b4943b5d9a6a783
tatyana86 <Krivonogova_1999@mail.ru>
2023-11-30 20:06:22 +0400

    create configuration mvc using java code

---
+++ spring-mvc-app1/src/main/java/ru/krivonogova/springcourse/config/MySpringMVCDispatcherSerlvetInitializer.java
@@ -0,0 +1,22 @@
+package ru.krivonogova.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherSerlvetInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {SpringConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String [] {"/"};
+	}
+
+}
