5ccd556c3968e0430e692a4bf4c8a281bf16c094
tatyana86 <Krivonogova_1999@mail.ru>
2023-12-01 21:41:07 +0400

    update contollers

---
+++ spring-mvc-app1/src/main/java/ru/krivonogova/springcourse/controllers/secondController.java
@@ -0,0 +1,13 @@
+package ru.krivonogova.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class secondController {
+
+	@GetMapping("/exit")
+	public String exit() {
+		return "second/exit";
+	}
+}
