07970392f962f8922fc11e1f1172d6da041ebf4d
tatyana86 <Krivonogova_1999@mail.ru>
2023-12-26 21:29:33 +0400

    simple spring-boot-app

---
+++ springcourse/src/main/java/ru/krivonogova/springcourse/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package ru.krivonogova.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@Value("${hello}")
+	private String hello;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		System.out.println(this.hello);
+		return "hello";
+	}
+	
+	
+
+}
---
+++ springcourse/src/test/java/ru/krivonogova/springcourse/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krivonogova.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
