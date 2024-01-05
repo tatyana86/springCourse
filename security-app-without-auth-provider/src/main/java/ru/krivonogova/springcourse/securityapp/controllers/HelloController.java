6884bc10e34bf1c99aa5c31efcf1ed5a24c89b09
tatyana86 <Krivonogova_1999@mail.ru>
2024-01-05 21:40:13 +0400

    only personDetailsService

---
+++ security-app-without-auth-provider/src/main/java/ru/krivonogova/springcourse/securityapp/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package ru.krivonogova.springcourse.securityapp.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ru.krivonogova.springcourse.securityapp.security.PersonDetails;
+
+@Controller
+public class HelloController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+	
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
+    }
+}
---
+++ security-app-without-auth-provider/src/main/java/ru/krivonogova/springcourse/securityapp/services/PersonDetailsService.java
@@ -0,0 +1,35 @@
+package ru.krivonogova.springcourse.securityapp.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import ru.krivonogova.springcourse.securityapp.models.Person;
+import ru.krivonogova.springcourse.securityapp.repositories.PeopleRepository;
+import ru.krivonogova.springcourse.securityapp.security.PersonDetails;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+	
+	private final PeopleRepository peopleRepository;
+
+	@Autowired
+	public PersonDetailsService(PeopleRepository peopleRepository) {
+		this.peopleRepository = peopleRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Person> person = peopleRepository.findByUsername(username);
+		
+		if(person.isEmpty()) {
+			throw new UsernameNotFoundException("User not found!");
+		}
+		
+		return new PersonDetails(person.get());
+	}
+}
