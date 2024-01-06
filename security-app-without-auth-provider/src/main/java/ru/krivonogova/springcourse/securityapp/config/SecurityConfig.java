f67a14700f75f4d7e01a07b0fc78bdea1802615e
tatyana86 <Krivonogova_1999@mail.ru>
2024-01-06 13:18:46 +0400

    add registration and bcryptPasswordEncoder

--- security-app-without-auth-provider/src/main/java/ru/krivonogova/springcourse/securityapp/config/SecurityConfig.java
+++ security-app-without-auth-provider/src/main/java/ru/krivonogova/springcourse/securityapp/config/SecurityConfig.java
@@ -3,9 +3,13 @@ package ru.krivonogova.springcourse.securityapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import ru.krivonogova.springcourse.securityapp.services.PersonDetailsService;
@@ -21,10 +25,41 @@ public class SecurityConfig {
 		this.personDetailsService = personDetailsService;
 	}
 	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Замените NoOpPasswordEncoder.getInstance() на вашу реализацию PasswordEncoder
+        return new BCryptPasswordEncoder();
+    }
+	
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
 		authenticationManagerBuilder.userDetailsService(personDetailsService);
+
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+		
+//		http.csrf(AbstractHttpConfigurer::disable);
+		
+        http.authenticationManager(authenticationManager)
+        .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/", "/auth/**").permitAll()
+                .anyRequest().authenticated()
+        );
+		
+        http.formLogin((formLogin) ->
+        formLogin
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error")
+        		);
+        
+        http.logout((logout) ->
+        logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login")
+        		);
+		
 		return http.build();
 	}
 
